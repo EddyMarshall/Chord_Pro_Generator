d50bcdb14fa0153409f2598369b05cde73c59812
mordes89 <mordes89@gmail.com>
2021-09-21 15:02:20 -0400

    Heroku please work

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require("express");
 const mongoose = require('mongoose');
 const users = require("./routes/api/users");
@@ -22,3 +24,10 @@ app.use("/api/songs", songs);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}

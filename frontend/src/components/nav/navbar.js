e6f3cc1e581a0da08e6281fddb1fd2084b1e1124
EvanCzako <evan.czako@gmail.com>
2021-09-20 20:37:33 -0400

    Complete user auth

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom'
-import './navbar.css'
+// import './navbar.css'
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -21,9 +21,9 @@ class NavBar extends React.Component {
       if (this.props.loggedIn) {
         return (
             <div>
-                <Link to={'/tweets'}>All Tweets</Link>
+                <Link to={'/songs'}>All Songs</Link>
                 <Link to={'/profile'}>Profile</Link>
-                <Link to={'/new_tweet'}>Write a Tweet</Link>
+                <Link to={'/new_song'}>Make a Song</Link>
                 <button onClick={this.logoutUser}>Logout</button>
             </div>
         );
@@ -40,7 +40,7 @@ class NavBar extends React.Component {
   render() {
       return (
         <div>
-            <h1>Chirper</h1>
+            <h1>User Page</h1>
             { this.getLinks() }
         </div>
       );
--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -6,7 +6,7 @@ import SignupForm from './signup_form';
 
 const mapStateToProps = (state) => {
   return {
-    signedIn: state.session.isSignedIn,
+    signedIn: state.session.isAuthenticated,
     errors: state.errors.session
   };
 };

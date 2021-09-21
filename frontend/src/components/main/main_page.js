e10536149abd4a53ba41b28aff3a77684d9ac842
Amin Babar <amin.babar1@gmail.com>
2021-09-20 20:52:47 -0400

    add Eddy's changes, copyrights

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -7,9 +7,8 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>A Twitter Clone</h1>
         <footer>
-          Copyright &copy; 2019 Chirper
+          Copyright AMEE production
         </footer>
       </div>
     );

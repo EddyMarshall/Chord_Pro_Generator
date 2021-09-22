df71b9573580b847481121c5677ba03b53b6b66b
mordes89 <mordes89@gmail.com>
2021-09-21 21:47:28 -0400

    changed app.js

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hi World"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+} else {
+  app.get("/", (req, res) => res.send("Hi World"));
+}
+
 app.use("/api/users", users);
 app.use("/api/songs", songs);
 
@@ -24,9 +32,3 @@ app.use("/api/songs", songs);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));  
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}

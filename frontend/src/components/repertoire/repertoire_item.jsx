17ebfae82f8699d331aa675dcdaa8b030d94e4a3
mordes89 <mordes89@gmail.com>
2021-09-23 20:17:02 -0400

    nothing yet

--- frontend/src/components/repertoire/repertoire_item.jsx
+++ frontend/src/components/repertoire/repertoire_item.jsx
@@ -6,7 +6,7 @@ export const RepertoireItem = props => {
     debugger
     return (
        <li className="repertoire-item">
-            <Link to={`songs/${props.song._id}`} className="repertoire-title">
+            <Link to={`/songs/${props.song._id}`} className="repertoire-title">
                 {props.song.title}
            </Link>
            <div className="repertoire-key">
@@ -14,6 +14,6 @@ export const RepertoireItem = props => {
            </div>  
        </li>
     )
-}
+};
 
-export default RepertoireItem
+export default RepertoireItem;

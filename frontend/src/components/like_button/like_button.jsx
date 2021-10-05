7159218afc8a5163af27338931c116bfa394a6c6
EvanCzako <evan.czako@gmail.com>
2021-10-05 13:20:05 -0400

    Add like and unlike functionality

--- frontend/src/components/like_button/like_button.jsx
+++ frontend/src/components/like_button/like_button.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import {getSongLikes, likeSong, unlikeSong} from '../../actions/like_actions';
 
 class LikeButton extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.handleLike = this.handleLike.bind(this);
+        // this.handleUnlike = this.handleUnlike.bind(this);
     }
 
     componentDidMount() {
-        // this.props.getSongLikes(this.props.songId);
+        this.props.getSongLikes(this.props.songId);
+    }
+
+    handleLike(){
+        this.props.likeSong({
+            liker: this.props.currentUser,
+            parent_song: this.props.songId
+        });
     }
 
     render() {
 
         let button = null;
         let likes = Object.values(this.props.likes);
-        button = <input type="button" value="like" className="like-button"/>;
+        button = <input type="button" value="like" className="like-button" onClick={this.handleLike}/>;
         for (let i = 0; i < likes.length; i++) {
             const like = likes[i];
             if(like.liker === this.props.currentUser){
-                button = <input type="button" value="unlike" className="unlike-button"/>;
+                button = <input type="button" value="unlike" className="unlike-button" onClick={() => this.props.unlikeSong(like._id)}/>;
             }
         }
 
@@ -33,13 +44,17 @@ class LikeButton extends React.Component {
 const mSTP = (state, ownProps) => {
     return {
         currentUser: state.session.user.id,
-        likes: state.entities.likes
+        likes: state.entities.likes,
+        songId: ownProps.match.params.songId
     }
 };
 
 const mDTP = dispatch => {
     return {
+        getSongLikes: (songId) => dispatch(getSongLikes(songId)),
+        likeSong: (like) => dispatch(likeSong(like)),
+        unlikeSong: (likeId) => dispatch(unlikeSong(likeId))
     }
 };
 
-export default connect(mSTP, mDTP)(LikeButton);
+export default withRouter(connect(mSTP, mDTP)(LikeButton));

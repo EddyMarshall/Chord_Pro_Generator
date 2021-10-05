940e215648dae75abb47f2b59ddbf1f0cfe999d3
your name hereEddy_Marshall <epmarsha2@gmail.com>
2021-10-05 15:32:43 -0400

    delete song function completed

--- frontend/src/components/repertoire/repertoire_item.jsx
+++ frontend/src/components/repertoire/repertoire_item.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { deleteSong } from '../../actions/song_actions';
 
-export const RepertoireItem = props => {
+class RepertoireItem extends React.Component {
 
-    return (
-       <li className="repertoire-item">
-            <Link to={`/songs/${props.song._id}`} className="repertoire-title">
-                {props.song.title}
-           </Link>
-           <div className="repertoire-key">
-                {`${props.song.key} Major`}
-           </div>  
-       </li>
+    constructor(props) {
+        super(props)
+        this.deleteSong = this.handleDelete.bind(this)
+    }
+
+    handleDelete(id) {
+        this.props.deleteSong(id)
+    }
+
+    render(){
+        return (
+            <li className="repertoire-item">
+            <Link to={`/songs/${this.props.song._id}`} className="repertoire-title">
+                {this.props.song.title}
+            </Link>
+            <button onClick={() => this.handleDelete(this.props.song._id)} className="song-delete-button">
+                Delete
+            </button>
+            <div className="repertoire-key">
+                {`${this.props.song.key} Major`}
+            </div>
+        </li>
     )
+}
 };
 
-export default RepertoireItem;
+const mapdispatchToProps = dispatch => ({
+    deleteSong: (id) => dispatch(deleteSong(id))
+})
+
+export default connect(null, mapdispatchToProps)(RepertoireItem)
